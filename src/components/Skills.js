5d87da4a653ef0b92b05fd900fb5acf6d8f18017
ArthurWSL <arthurhcbp@gmail.com>
2021-02-19 20:33:22 -0300

    terminado skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
+import html from '../svg/html.svg'
+import css from '../svg/css.svg'
+import js from '../svg/javascript.svg'
+import sass from '../svg/sass.svg'
+import react from '../svg/react.svg'
+import node from '../svg/node.svg'
+import git from '../svg/git.svg'
+
 function Skills() {
     return (
         <div>
-            <div className="container">
+            <div className="skills-container">
                 <h2>Skills</h2>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
+                <div className="skills-list">
+                    <img className="html" src={html} alt="html"/>
+                    <img className="css" src={css} alt="css"/>
+                    <img className="javascript" src={js} alt="javascript"/>
+                    <img className="sass" src={sass} alt="sass"/>
+                    <img className="react" src={react} alt="react"/>
+                    <img className="node" src={node} alt="node"/>
+                    <img className="git" src={git} alt="git"/>
+                </div>
+                    
             </div>
         </div>
     )

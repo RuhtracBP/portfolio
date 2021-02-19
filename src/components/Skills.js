61a113eee105fb569bfc8652a752c93e1af1fa21
ArthurWSL <arthurhcbp@gmail.com>
2021-02-19 16:49:53 -0300

    terminado about section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,7 @@ function Skills() {
     return (
         <div>
             <div className="container">
+                <h2>Skills</h2>
                 <img src="" alt=""/>
                 <img src="" alt=""/>
                 <img src="" alt=""/>
